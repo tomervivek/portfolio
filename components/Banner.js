5e3a874ef01a6d88d1ec86f555e17c812015ca6c
Vivek Singh <73754423+tomervivek@users.noreply.github.com>
2024-02-27 17:29:02 +0530

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -1,9 +1,21 @@
-import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
-import Header from "./Header";
+import { useEffect, useState } from "react";
 
 export default function Banner() {
+  const [loadingText, setLoadingText] = useState('V');
+  var loadingWord = 'Vivek Singh'
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setLoadingText(prevText => (prevText === 'Vivek Singh' ? 'V' : prevText + loadingWord[prevText.length]));
+    }, 300);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
 var arr=[ 7,1,2,3,4,5,"React","Javascript","NextJs","React Native","Python","Html","Html5","Css","Css3","Talwind Css","Git",5,5,5,6,7,7,1,2,3,4,5,6,7,8,2,3,4,5,5,5,6,7,7]
-  return (
+  
+
+return (
     
     <div className="bg-cover bg-center " >
     <div class="area">
@@ -18,8 +30,7 @@ var arr=[ 7,1,2,3,4,5,"React","Javascript","NextJs","React Native","Python","Htm
         <div className="mt-36 mx-auto max-w-screen-xl px-4 lg:py-8 sm:px-6  lg:px-8  ">
           <div className=" ">
             <h2 className="text-3xl font-bold tracking-tight text-[#5C8374] sm:text-4xl">
-              Vivek Singh 
-           
+              {loadingText}
             </h2>
             <div className="mt-2 text-lg mb-24   md:mb-0 leading-8  text-[#93B1A6]">
              
